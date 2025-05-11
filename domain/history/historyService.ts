d45ed33808aaff78fe4703674625674053d23970
“09B22071” <09B22071@localhost>
2025-05-11 21:18:07 +0900

    案をもとに再現

--- domain/history/historyService.ts
+++ domain/history/historyService.ts
@@ -2,5 +2,5 @@ import historyData from '@/infrastructure/data/history.json';
 import { HistoryEvent } from './history';
 
 export const getHistory = (): HistoryEvent[] => {
-  return historyData.history;
+  return historyData;
 }; 
